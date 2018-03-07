d6ac8283787c31c052533b2d164a36cd32b7b9b6
Aidan Woods <aidantwoods@gmail.com>
2018-03-07 18:16:04 +0000

    Add easy conversion to string

--- Sources/Paseto/StdLibExtensions/Data.swift
+++ Sources/Paseto/StdLibExtensions/Data.swift
@@ -13,6 +13,8 @@ extension Data {
         return Sodium().utils.bin2base64(self, variant: .URLSAFE_NO_PADDING)!
     }
 
+    var utf8String: String? { return String(data: self, encoding: .utf8) }
+
     init? (base64UrlNoPad encoded: String) {
         guard let data = Sodium().utils
             .base642bin(encoded, variant: .URLSAFE_NO_PADDING) else {
