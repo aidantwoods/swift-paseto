219f5f3c6f897238b1cd781ac8cfd353b182faee
Aidan Woods <aidantwoods@gmail.com>
2018-03-08 15:12:28 +0000

    Implement encrypt and decrypt for version2

--- Sources/Paseto/Implementations/Version2.swift
+++ Sources/Paseto/Implementations/Version2.swift
@@ -10,6 +10,63 @@ import Foundation
 public struct Version2: Implementation {
     public static var version: Version { return .v2 }
 
+    public static func encrypt(
+        _ message: Data, with key: SymmetricKey, footer: Data
+    ) -> Blob<EncryptedPayload> {
+        let nonceBytes = Int(Aead.nonceBytes)
+        let preNonce = sodium.randomBytes.buf(length: nonceBytes)!
+
+        let nonce = sodium.genericHash.hash(
+            message: message,
+            key: preNonce,
+            outputLength: nonceBytes
+        )!
+
+        let header = Header(version: version, purpose: .Local)
+
+        let cipherText = Aead.xchacha20poly1305_ietf_encrypt(
+            message: message,
+            additionalData: Util.pae([header.asData, nonce, footer]),
+            nonce: nonce,
+            secretKey: key.material
+        )!
+
+        let payload = EncryptedPayload(nonce: nonce, cipherText: cipherText)
+
+        return Blob(header: header, payload: payload, footer: footer)
+    }
+
+    public static func decrypt(
+        _ encrypted: Blob<EncryptedPayload>,
+        with key: SymmetricKey,
+        footer: Data
+    ) throws -> Data {
+        let header = encrypted.header
+
+        guard header == Header(version: version, purpose: .Local) else {
+            throw Exception.badHeader("Bad message header.")
+        }
+        guard footer == encrypted.footer else {
+            throw Exception.badFooter("Invalid message footer.")
+        }
+
+        let nonce      = encrypted.payload.nonce
+        let cipherText = encrypted.payload.cipherText
+
+        guard let message = Aead.xchacha20poly1305_ietf_decrypt(
+            cipherText: cipherText,
+            additionalData: Util.pae([header.asData, nonce, footer]),
+            nonce: nonce,
+            secretKey: key.material
+        ) else {
+            throw Exception.invalidSignature(
+                "The message could not be decrypted."
+            )
+        }
+
+        return message
+    }
+
     public static func sign(
         _ data: Data, with key: AsymmetricSecretKey, footer: Data
     ) -> Blob<SignedPayload> {
@@ -62,5 +119,6 @@ extension Version2 {
         case badHeader(String)
         case badFooter(String)
         case invalidSignature(String)
+        case invalidMessage(String)
     }
 }
