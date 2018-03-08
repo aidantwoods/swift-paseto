6c90400c4e3f7e5bfa403cb357715800eb5fac8f
Aidan Woods <aidantwoods@gmail.com>
2018-03-08 15:11:50 +0000

    Implement parsing of EncryptedPayload

---
+++ Sources/Paseto/Payloads/EncryptedPayload.swift
@@ -0,0 +1,33 @@
+//
+//  EncryptedPayload.swift
+//  Paseto
+//
+//  Created by Aidan Woods on 08/03/2018.
+//
+
+import Foundation
+
+public struct EncryptedPayload {
+    let nonce: Data
+    let cipherText: Data
+
+    init (nonce: Data, cipherText: Data) {
+        self.nonce      = nonce
+        self.cipherText = cipherText
+    }
+}
+
+extension EncryptedPayload: Payload {
+    public var asData: Data { return nonce + cipherText }
+
+    public init? (data: Data) {
+        let nonceLen = Int(Aead.nonceBytes)
+
+        guard data.count > nonceLen else { return nil }
+
+        self.init(
+            nonce:      data[..<nonceLen],
+            cipherText: data[nonceLen...]
+        )
+    }
+}
