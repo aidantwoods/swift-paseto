e7b045cb1db9effeaf7e6fc71065078a4ebd00bc
Aidan Woods <aidantwoods@gmail.com>
2018-03-08 15:14:12 +0000

    Check encrypted payload can be decrypted

--- Tests/PasetoTests/Version2Test.swift
+++ Tests/PasetoTests/Version2Test.swift
@@ -51,5 +51,28 @@ class Version2Test: XCTestCase {
 
         XCTAssertEqual(message, "Foobar!")
     }
+
+    func testEncrypt() {
+        let sk = SymmetricKey()
+
+        let message = """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            pretium orci enim, tincidunt bibendum diam suscipit et.
+            Pellentesque vel sagittis sem, vitae tempor elit. Sed non suscipit
+            augue. In hac habitasse platea dictumst. Nunc consectetur et urna
+            ac molestie. Nunc eleifend nisi nisl, non ornare nunc auctor sit
+            amet. Sed eu sodales nibh. Etiam eros mi, molestie in nibh in,
+            cursus ullamcorper augue. Duis id vestibulum nulla. Nulla in
+            fermentum arcu. Nunc et nibh nec lacus pellentesque vulputate
+            commodo vel sapien. Sed molestie, dui ac condimentum feugiat, magna
+            risus tincidunt est, feugiat faucibus est magna at arcu. 👻
+            """
+
+        let encryptedBlob = Version2.encrypt(message, with: sk)
+
+        let decrypted: String = Version2.decrypt(encryptedBlob, with: sk)!
+
+        XCTAssertEqual(message, decrypted)
+    }
 }
 
