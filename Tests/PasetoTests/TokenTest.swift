8540351dc8c5cbd3aeb868ca7f3db5c567f26954
Aidan Woods <aidantwoods@gmail.com>
2018-03-09 01:47:50 +0000

    Test blob is decrypted to token correctly

--- Tests/PasetoTests/TokenTest.swift
+++ Tests/PasetoTests/TokenTest.swift
@@ -9,7 +9,34 @@ import XCTest
 import Paseto
 
 class TokenTest: XCTestCase {
-    func testLocal() {
+    func testDecrypt() {
+        let key = try! SymmetricKey<Version2>(
+            encoded: "cHFyc3R1dnd4eXp7fH1-f4CBgoOEhYaHiImKi4yNjo8"
+        )
+
+        let blob = Blob<Encrypted>("""
+            v2.local.lClhzVOuseCWYep44qbA8rmXry66lUupyENijX37_I_z34EiOlfyuwqII
+            hOjF-e9m2J-Qs17Gs-BpjpLlh3zf-J37n7YGHqMBV6G5xD2aeIKpck6rhfwHpGF38L
+            7ryYuzuUeqmPg8XozSfU4PuPp9o8.UGFyYWdvbiBJbml0aWF0aXZlIEVudGVycHJpc
+            2Vz
+            """.replacingOccurrences(of: "\n", with: "")
+        )!
+
+        let token = blob.decrypt(with: key)!
+
+        let expectedClaims = [
+            "data": "this is a signed message",
+            "expires": "2019-01-01T00:00:00+00:00",
+        ]
+        XCTAssertEqual(expectedClaims, token.claims)
+
+        let expectedFooter = "Paragon Initiative Enterprises"
+        XCTAssertEqual(expectedFooter, token.footer)
+
+        XCTAssertEqual(type(of: key).version, .v2)
+    }
+
+    func testEncrypt() {
         let token = Token(claims: ["foo": "bar"])
             .replace(allowedVersions: [.v2])
             .replace(footer: "There be secrets within...")
