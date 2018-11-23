065cb84989967f4a36217e30185d133d236ceb7c
Aidan Woods <aidantwoods@gmail.com>
2018-11-23 20:49:48 +0000

    Explicitly provide this initialiser (Swift 4.2)

--- Sources/Paseto/BytesRepresentables/Data+Extension.swift
+++ Sources/Paseto/BytesRepresentables/Data+Extension.swift
@@ -9,4 +9,8 @@ import Foundation
 
 extension Data: PureBytesRepresentable, BytesRepresentable {
     public var bytes: Bytes { return Bytes(self) }
+
+    public init (bytes: Bytes) {
+        self.init(bytes)
+    }
 }
