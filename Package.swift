96a6edd3645375feaeac4dd176ef3de86c92786c
Aidan Woods <git@public.aidanwoods.com>
2022-05-23 00:33:34 +0100

    Drop support for 2019 OSs
    These don't appear to have proper support for x963Representation.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ import PackageDescription
 let package = Package(
     name: "Paseto",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+        .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)
     ],
     products: [
         .library(name: "Paseto", targets: ["Paseto"]),
