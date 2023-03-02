7df0c5fcc6732afc0362575529975d00ec24e25a
Aidan Woods <git@public.aidanwoods.com>
2023-03-02 00:23:04 +0000

    Add manual decompression logic
    Apple seeimingly broke the x963Representation constructor when releasing
    the new compressedRepresentaion constructor, the former no longer accepts
    compressed points which is a BC break. This would be fine, except the new
    method cannot be used by newer OS versions if we want to support older OS
    versions at the same time. Either we need to avoid using the x963
    constructor, or only support macOS 13 and similar. This is pretty new,
    and not even GitHub supports it yet for pipelines. It's not ideal, but
    it seems the only workable alternative is the implement handling of
    compressed points manually. This is annoted pretty heavily to follow the
    steps and notation in the spec.

--- Sources/Paseto/Implementations/Version3/Public/V3PublicAsymmetricPublicKey.swift
+++ Sources/Paseto/Implementations/Version3/Public/V3PublicAsymmetricPublicKey.swift
@@ -1,5 +1,26 @@
 import Foundation
 import CryptoKit
+import CryptoSwift
+
+fileprivate let zeroBn = BigUInteger(0)
+fileprivate let oneBn = BigUInteger(1)
+fileprivate let twoBn = BigUInteger(2)
+fileprivate let fourBn = BigUInteger(4)
+
+// The following consts are extracted from the NIST spec.
+// https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-186-draft.pdf
+
+// p = 2^384 − 2^128 − 2^96 + 2^32 − 1
+fileprivate let pBn = twoBn.power(384) - twoBn.power(128) - twoBn.power(96) + twoBn.power(32) - 1
+
+// a = -3 mod p
+//   = 3940200619639447921227904010014361380507973927046544666794\
+//     8293404245721771496870329047266088258938001861606973112316
+fileprivate let aBn = BigUInteger("39402006196394479212279040100143613805079739270465446667948293404245721771496870329047266088258938001861606973112316", radix: 10)!
+
+// b = 2758019355995970587784901184038904809305690585636156852142\
+//     8707301988689241309860865136260764883745107765439761230575
+fileprivate let bBn = BigUInteger("27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575", radix: 10)!
 
 @available(macOS 11, iOS 14, watchOS 7, tvOS 14, macCatalyst 14, *)
 extension Version3.Public {
@@ -7,6 +28,60 @@ extension Version3.Public {
         public static let length = 49
 
         let key: P384.Signing.PublicKey
+
+        fileprivate static func decompressToCoords(compressedKey: Bytes) throws -> (x: Bytes, y: Bytes) {
+            guard compressedKey.count == 1 + 48 else {
+                throw Exception.badKey("Bad public key length")
+            }
+
+            if compressedKey == Bytes(repeating: 0, count: 49) {
+                return (x: Bytes(repeating: 0, count: 48), y: Bytes(repeating: 0, count: 48))
+            }
+
+            let prefix = compressedKey[0]
+            let x = compressedKey[1...].bytes
+
+            let yTildeP: BigUInteger
+            switch prefix {
+            case 02:
+                yTildeP = zeroBn
+            case 03:
+                yTildeP = oneBn
+            default:
+                throw Exception.badKey("Bad public key prefix")
+            }
+
+            let xBn = BigUInteger(Data(x))
+
+            let alpha = (xBn.power(3) + (aBn * xBn) + bBn) % pBn
+
+            // Take square root mod p
+            // https://en.wikipedia.org/wiki/Tonelli%E2%80%93Shanks_algorithm
+            // For prime p = 3 (mod 4), r = n ^ ((p+1)/n) (mod p) is a root of r^2 = n (mod p)
+            let beta = alpha.power((pBn + 1)/4, modulus: pBn)
+
+            let yBn: BigUInteger
+            if beta % 2 == yTildeP {
+                yBn = beta
+            } else {
+                yBn = pBn - beta
+            }
+
+            let y = Bytes(yBn.serialize())
+
+            guard y.count <= 48 else {
+                throw Exception.badKey("Invalid y byte length")
+            }
+
+            let yPadded = Bytes(repeating: 0, count: 48 - y.count) + y
+
+            return (x, yPadded)
+        }
+
+        fileprivate static func decompress(compressedKey: Bytes) throws -> P384.Signing.PublicKey {
+            let (x, y) = try decompressToCoords(compressedKey: compressedKey)
+            return try P384.Signing.PublicKey(rawRepresentation: x + y)
+        }
 
         fileprivate static func compress(key: P384.Signing.PublicKey) -> Bytes {
             let x963Representation = key.x963Representation.bytes
@@ -64,22 +139,7 @@ extension Version3.Public {
                 )
             }
 
-            if #available(macOS 13, iOS 16, watchOS 9, tvOS 16, macCatalyst 16, *) {
-                guard
-                    let key = try? P384.Signing.PublicKey(compressedRepresentation: material) else {
-                    throw Exception.badKey("Public key is invalid")
-                }
-                self.key = key
-            } else {
-                // Note that this would actually fail on newer versions, so it seems there was a BC break
-                // here which means this constructor will no longer accept compressed points like it once
-                // did.
-                guard
-                    let key = try? P384.Signing.PublicKey(x963Representation: material) else {
-                    throw Exception.badKey("Public key is invalid")
-                }
-                self.key = key
-            }
+            self.key = try Self.decompress(compressedKey: material)
         }
 
         init (_ key: P384.Signing.PublicKey) {
