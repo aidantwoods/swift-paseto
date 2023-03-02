955836efa7c9d2e9b2f61bc28c5a6b3c46fb3e6b
Aidan Woods <git@public.aidanwoods.com>
2023-03-02 00:31:41 +0000

    Explicit skip on old OS

--- Tests/PasetoTests/KeyTest.swift
+++ Tests/PasetoTests/KeyTest.swift
@@ -5,13 +5,17 @@ import Sodium
 
 class KeyTest: XCTestCase {
     func testInvalidKeyImport() {
-        let material = sodium.utils.hex2bin( "04fbcb7c69ee1c60579be7a334134878d9c5c5bf35d552dab63c0140397ed14cef637d7720925c44699ea30e72874c72fbfbcb7c69ee1c60579be7a334134878d9c5c5bf35d552dab63c0140397ed14cef637d7720925c44699ea30e72874c72fb")!
-        
-        // import invalid key
-        let pubKey = try! P384.Signing.PublicKey(x963Representation: material)
-        
-        // should detect invalid key
-        XCTAssertThrowsError(try Paseto.Version3.AsymmetricPublicKey(key: pubKey))
+        if #available(macOS 11, iOS 14, watchOS 7, tvOS 14, macCatalyst 14, *) {
+            let material = sodium.utils.hex2bin( "04fbcb7c69ee1c60579be7a334134878d9c5c5bf35d552dab63c0140397ed14cef637d7720925c44699ea30e72874c72fbfbcb7c69ee1c60579be7a334134878d9c5c5bf35d552dab63c0140397ed14cef637d7720925c44699ea30e72874c72fb")!
+
+            // import invalid key
+            let pubKey = try! P384.Signing.PublicKey(x963Representation: material)
+
+            // should detect invalid key
+            XCTAssertThrowsError(try Paseto.Version3.AsymmetricPublicKey(key: pubKey))
+        } else {
+            _ = XCTSkip("Skipping key test where key is not supported")
+        }
     }
 }
 
